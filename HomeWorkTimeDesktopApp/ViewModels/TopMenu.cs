48ee53aff6d87bec53298f02e1cb5116594b2b72
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2026-05-31 14:09:46 -0300

    Adicionar arquivos de projeto.

---
+++ HomeWorkTimeDesktopApp/ViewModels/TopMenu.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+
+namespace HomeWorkTimeDesktopApp.ViewModels;
+
+public partial class TopMenu : ObservableObject
+{
+    private bool _isDebbugMode = Debugger.IsAttached;
+
+    [ObservableProperty]
+    private bool closeVisible = true;
+    [ObservableProperty]
+    private bool minimizeVisible = true;
+    [ObservableProperty]
+    private bool maximizeVisible = true;
+    [ObservableProperty]
+    private bool configVisible = false;
+
+    public TopMenu()
+    {
+         
+    }
+}
