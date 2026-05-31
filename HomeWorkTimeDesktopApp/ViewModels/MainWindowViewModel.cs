e885882042c4e6114b0ed54f8aa1077c0bb29dcb
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2026-05-31 14:40:51 -0300

    Add: Timer (front-end)

---
+++ HomeWorkTimeDesktopApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace HomeWorkTimeDesktopApp.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    private System.Timers.Timer _workedTimer = new System.Timers.Timer(333);
+    private System.Timers.Timer _paidPausedTimer = new System.Timers.Timer(333);
+    private System.Timers.Timer _unPaidTimer = new System.Timers.Timer(333);
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(TotalWorkedTimeText))]
+    private TimeSpan totalWorkedTime = TimeSpan.Zero;
+    [ObservableProperty]
+    private TimeSpan totalPaidPausedTime = TimeSpan.Zero;
+    [ObservableProperty]
+    private TimeSpan currentlyPaidPausedTime = TimeSpan.Zero;
+    [ObservableProperty]
+    private TimeSpan totalUnaidPausedTime = TimeSpan.Zero;
+    [ObservableProperty]
+    private TimeSpan currentlyUnpaidPausedTime = TimeSpan.Zero;
+    
+    public string MainTitle { get; set; }
+    public string TotalWorkedTimeText { get { return $"{TotalWorkedTime.Hours}:{TotalWorkedTime.Minutes}"; } }
+
+
+    public MainWindowViewModel()
+    {
+        MainTitle = Debugger.IsAttached ? "HomeWorkTimeDesktopApp (Debug)" : "HomeWorkTimeDesktopApp";
+    }
+}
