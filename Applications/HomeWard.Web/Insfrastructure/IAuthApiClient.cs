a1701b440c6e9f2a02d0f123d0602c330579c54e
rafaelEleuterio <rafale.eleuteiro.sp@gmail.com>
2026-06-14 14:01:03 -0300

    Add: Initial Blazor WebApp

---
+++ Applications/HomeWard.Web/Insfrastructure/IAuthApiClient.cs
@@ -0,0 +1,8 @@
+﻿using HomeWard.Application.Auth;
+
+namespace HomeWard.Web.Insfrastructure;
+
+public interface IAuthApiClient
+{
+    Task<LoginResult> LoginAsync(string username, string password, CancellationToken cancellationToken = default);
+}
